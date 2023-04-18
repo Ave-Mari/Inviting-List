2d9a52f77e92017ab9c45985fd79717d4ae1b1b9
Mari Vaypan <avemari98@gmail.com>
2023-04-18 23:58:58 +0300

    add style to pics and names

---
+++ src/components/FriendsList/FriendsList.styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.section`
+
+        ul {
+            display: flex;
+
+            li {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+            }
+
+            img {
+                width: 90px;
+                height: 90px;
+                border-radius: 50%;
+                object-fit: cover;
+            }
+        }
+    
+
+`
