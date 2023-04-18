04bb7b73dacbcf1fedd24dfe64eb9f0a09dc9a77
Mari Vaypan <avemari98@gmail.com>
2023-04-18 22:34:52 +0300

    add app.tsx and basic class component

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Component } from 'react'; 
+
+export default class App extends Component {
+  render() {
+
+    return (
+
+      
+      <section>App</section>
+    )
+  }
+}
