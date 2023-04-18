3ba6b83142587eb79c82b6af75a89edbc5842a8d
Mari Vaypan <avemari98@gmail.com>
2023-04-18 22:33:08 +0300

    add gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,6 @@ export default class App extends Component {
 <>
       <section>App</section>
       <section>App</section>
-      <section>App</section>
-      <section>App</section>
       </>
     )
   }
