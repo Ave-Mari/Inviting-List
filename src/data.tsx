5b9722ec7b4fcdf43f38ed9fbfc054fb61f27fa8
Mari Vaypan <avemari98@gmail.com>
2023-04-18 22:35:10 +0300

    add data with friend list

---
+++ src/data.tsx
@@ -0,0 +1,44 @@
+type friendsData = {name: string, pic: string}[]
+
+const friendsData = [
+    {
+        name: 'Paul Black',
+        pic: 'https://img.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg?t=st=1681845876~exp=1681846476~hmac=b717d86f7e5743e5f2c9aae4c8e1edb7523a071f71393cc6f201b4593974d33e'
+    },
+    {
+        name: 'Jack Stoll',
+        pic: 'https://img.freepik.com/free-photo/young-businessman-siting-cafeteria-with-laptop-computer-table_342744-455.jpg?w=996&t=st=1681845976~exp=1681846576~hmac=459d4d3c719b6f17dceefbbed391a0fd69e91db87a2e0818048ab27ac0e01526'
+    },
+    {
+        name: 'Jenny Curran',
+        pic: 'https://img.freepik.com/free-photo/positive-carefree-european-woman-dances-with-rhythm-music-listens-favorite-song-via-headphones-wears-knitted-orange-sweater-smiles-broadly-isolated-purple-background-foolishes-around_273609-58571.jpg?w=996&t=st=1681845876~exp=1681846476~hmac=2b555bee01d67895b4c15336f5e75c4b27855c06fcfe18e6b084c3edd6d93339'
+    },
+    {
+        name: 'Oskar Madden',
+        pic: 'https://img.freepik.com/premium-photo/young-attractive-man-with-paper-cup-coffee-car_152898-4130.jpg'
+    },
+    {
+        name: 'Monica Beam',
+        pic: 'https://img.freepik.com/free-photo/portrait-good-looking-cheerful-african-american-woman-enjoys-company-small-pedigree-dog-wears-orange-jumper-spends-free-time-with-favorite-pet-isolated-white-wall-animal-owner_273609-49840.jpg?w=996&t=st=1681845905~exp=1681846505~hmac=bcac9b623ef42cc7bf7e4e2bf1587627dcc470a18f4be5ef4c6b9476b39cb305'
+    },
+    {
+        name: 'Leo Finch',
+        pic: 'https://img.freepik.com/free-photo/caucasian-pretty-girl-walking-around-city-with-boyfriend-smiling-brunette-man-spending-weekend-with-girlfriend_197531-12261.jpg'
+    },
+    {
+        name: 'Emma Smith',
+        pic: 'https://img.freepik.com/free-photo/beautiful-female-office-worker-carrying-out-administrative-work-company_197531-4960.jpg?w=996&t=st=1681846127~exp=1681846727~hmac=1148ee6c605af034f5e69864589b48ba58a9d58f5942ca7dba6a28988923a5b1'
+    },
+    {
+        name: 'Henry Johnson',
+        pic: 'https://img.freepik.com/free-photo/guy-resting-his-head-his-hands-metal-staircase-yellow-door_181624-45375.jpg?t=st=1681846052~exp=1681846652~hmac=9415575fca03c43b542d80cff60b5da2feaeba94a9af39e7474def43815586b1'
+    },
+    {
+        name: 'Sam Miller',
+        pic: 'https://img.freepik.com/premium-photo/cheerful-smiling-man-casual-attire-is-feeling-happy-when-holding-his-adorable-dog-york-terrier_245974-10637.jpg?w=360'
+    },
+    {
+        name: 'Liam Bush',
+        pic: 'https://img.freepik.com/free-photo/cool-guy-shows-his-finger-up-listens-music-orange-background-portrait-darkskinned-man-blue-tee-dancing-isolated-backdrop_197531-28583.jpg?w=996&t=st=1681846111~exp=1681846711~hmac=575b88311ac9d7efa025f30c6f0febe20cb2e34d479091ccc0adbf08ca1a304c'
+    },
+]
